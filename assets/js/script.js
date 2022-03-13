1354d95a66e3f6c5ac7231a8ceaaa5514ef99daf
CassDoes <c.doexo@gmail.com>
2022-03-13 11:54:33 -0500

    save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
 var todayEl = document.querySelector('#currentDay');
-        todayEl.innerHTML = moment().format("MMMM Do, YYYY");
+todayEl.innerHTML = moment().format("MMMM Do, YYYY");
+
+
+document.querySelector(".saveBtn").addEventListener("click", function () {
+  var textInput = document.querySelector(".textarea").value ;
+  localStorage.setItem("textarea", textInput) ;
+});
+
+
 
 var hour9 = document.querySelector(".h-9");
+  hour9.textContent = moment("9", "h").format("ha");
 var hour10 = document.querySelector(".h-10");
 var hour11 = document.querySelector(".h-11");
 var hour12 = document.querySelector(".h-12");
@@ -11,41 +20,46 @@ var hour15 = document.querySelector(".h-15");
 var hour16 = document.querySelector(".h-16");
 var hour17 = document.querySelector(".h-17");
 
-
 let hourArray = [
-        hour9 = "9",
-        hour10 = "10",
-        hour11 = "11",
-        hour12 = "12",
-        hour13 = "13",
-        hour14 = "14",
-        hour15 = "15",
-        hour16 = "16",
-        hour17 = "17"
+  hour9 = moment("9", "h").format("h"),
+  hour10 = "10",
+  hour11 = 11,
+  hour12 = 12,
+  hour13 = 13,
+  hour14 = 14,
+  hour15 = moment("15", "ha").format("ha"),
+  hour16 = 16,
+  hour17 = 17
 ];
-
-console.log(hourArray);
-
 
 
 var timeEl = function() {
-        var getTime = moment().format("H");
-        for (i = 0; i < hourArray.length; i++) {
-                console.log(hourArray[i]);
-                console.log(getTime);
-        }
-                if (getTime === hourArray[i]) {
-                       hourArray.addClass("present");
-                }
-                else if (getTime < hourArray[i]) {
-                        hourArray[i].addClass("past");
-                }
+
+  let getTime = moment().format("h");
+  console.log(getTime);
+
+  var hourArray = $(".hour");
+
+  for (i = 0; i < hourArray.length; i++) {
+    let hour = $(hourArray[i]).attr('id');
+    console.log(hour);
+
+    
+    if (getTime < hourArray[i]) {
+      hourArray.addClass("past");
+      console.log("past");
+    } 
+    else if (getTime === hourArray[i]) {
+      $(hourArray[i]).addClass("present");
+      console.log("present");
+    } 
+    else {
+      $(hourArray[i]).addClass("future");
+      console.log("future");
+    }
+  }
 }
 
 timeEl();
 
-
-
-//moment("17", "H").format("H")
-
-
+//let difference = moment(hour, 'ha').diff(moment(getTime, 'ha'), 'hours');
